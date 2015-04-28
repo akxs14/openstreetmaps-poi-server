89cfaba36c725ce97fca2e5f5143f00b557a5ce2
angelos <angelos.kapsimanis@gmail.com>
2015-04-28 02:03:35 +0200

    Add gas station schema

---
+++ sql/gas_stations.sql
@@ -0,0 +1,53 @@
+-- Table: gas_stations
+
+DROP TABLE gas_stations;
+
+CREATE TABLE gas_stations
+(
+  id serial NOT NULL,
+  lat text,
+  lon text,
+  brand text,
+  operator text,
+  name text,
+  addr_country text,  
+  addr_city text,  
+  addr_street text,
+  addr_housenumber text,  
+  addr_postcode text,
+  phone text,
+  shop text,
+  wheelchair text,
+  opening_hours text,
+  payment_cash text,
+  payment_mastercard text,
+  payment_visa text,
+  payment_maestro text,
+  payment_dkv text,
+  payment_uta text,
+  fuel_diesel text,
+  fuel_gtl_diesel text,
+  fuel_hgv_diesel text,    
+  fuel_octane_91 text,
+  fuel_octane_95 text,
+  fuel_octane_98 text,
+  fuel_octane_100 text,
+  fuel_octane_102 text,  
+  fuel_1_25 text,
+  fuel_1_50 text,
+  fuel_biodiesel text,
+  fuel_svo text,
+  fuel_e10 text,
+  fuel_e85 text,
+  fuel_biogas text,
+  fuel_lpg text,
+  fuel_cng text,
+  fuel_LH2 text,
+  fuel_adblue text,  
+  CONSTRAINT gas_stations_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE gas_stations
+  OWNER TO postgres;
