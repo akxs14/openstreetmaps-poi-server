506b2f6346b5d1e88a85c7d3e32fe48d1bebee43
angelos <angelos.kapsimanis@gmail.com>
2015-04-28 23:17:08 +0200

    Get query string parameters

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
+require 'json'
 
 get '/hi' do
   "Hello!"
+end
+
+get "/poi/:type" do
+  content_type :json
+  puts "params: #{params}"
+  puts "query string: #{request.query_string}"
+
+  {
+    "type" => params['type'],
+    "center_lat" => params[:center_lat],
+    "center_lon" => params[:center_lon],
+    "radius" => params[:radius]
+   }.to_json
 end
---
+++ curl/gas_stations.sh
@@ -0,0 +1,3 @@
+curl -X GET http://localhost:4567/poi/gas_station?center_lat=X&center_lon=Z&radius=1 \
+        --data '{}' \
+        --header "Content-type:application/json"
